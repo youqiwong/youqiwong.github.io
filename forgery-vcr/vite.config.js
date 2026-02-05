0870ba8a23406968f985564292a81c215b248665
youqiwang <youqiwang@tencent.com>
2026-02-05 14:52:18 +0800

    Setup project page with password protection

--- forgery-vcr/vite.config.js
+++ forgery-vcr/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ForgeryVCR/', // GitHub 仓库名
+  // 开发环境用 /，生产环境用 /projects/ForgeryVCR/
+  base: process.env.NODE_ENV === 'production' ? '/projects/ForgeryVCR/' : '/',
 })
